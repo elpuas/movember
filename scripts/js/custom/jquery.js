b43cb58a146c9f46bdc125b0729eb24df2ba290e
Alfredo Navas <elpuas@gmail.com>
2019-11-01 13:09:47 -0600

    snapshot 11-01-1:09PM

--- scripts/js/custom/jquery.js
+++ scripts/js/custom/jquery.js
@@ -1,6 +1,31 @@
 /* eslint-disable */
 jQuery(document).ready(function ($) {
-	
+	const checkboxPilsen = $( 'input[name=pilsen]' );
+	const checkboxFans = $( 'input[name=fans]' );
+	const instagramPilsen = $('#plsn-instagram-pilsen');
+	const instagramFans = $('#plsn-instagram-fans');
+	const btnPilsen = $('#plsn-list');
+	const btnFans = $('#fns-list');
+
+	if( checkboxPilsen.length) {
+		$(checkboxPilsen).attr('checked', 'checked');
+		$(instagramPilsen).css('display', 'block');
+		$(instagramFans).css('display', 'none');
+
+		$(btnPilsen).on('click', function(){
+			$(checkboxPilsen).attr('checked', 'checked');
+			$(checkboxFans).removeAttr('checked');
+			$(instagramPilsen).css('display', 'block');
+			$(instagramFans).css('display', 'none');
+		})
+
+		$(btnFans).on('click', function(){
+			$(checkboxFans).attr('checked', 'checked');
+			$(checkboxPilsen).removeAttr('checked');
+			$(instagramFans).css('display', 'block');
+			$(instagramPilsen).css('display', 'none');
+		})
+	}
 })
 
 
